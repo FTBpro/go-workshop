9b54e9b042332ac1a70643f920d61d8e0173f8e8
avihay <avihay@90min.com>
2019-04-26 13:24:50 +0300

    add write rout

--- coolfacts/cmd/main.go
+++ coolfacts/cmd/main.go
@@ -14,7 +14,7 @@ import (
 var Cache = Store{}
 
 type Store struct {
-	Data  []MF
+	Data []MF
 }
 
 type MF struct {
@@ -23,11 +23,17 @@ type MF struct {
 	FactId        string   `json:"factId"`
 	Headline      string   `json:"headline"`
 	ShortHeadline string   `json:"shortHeadline"`
-	Fact          string   `json:"fact"`
+	FactText      string   `json:"fact"`
 	FullStoryUrl  string   `json:"fullStoryUrl"`
 	Tags          []string `json:"tags"`
 	PrimaryImage  string   `json:"primaryImage"`
 	ImageCredit   string   `json:"imageCredit"`
+}
+
+type Fact struct {
+	Image       string
+	Url         string
+	Description string
 }
 
 var newsTemplate = `<html>
@@ -35,7 +41,7 @@ var newsTemplate = `<html>
                     <div>
                         {{range .}}
                             <div>
-                                <h3>{{.Fact}}</h3>
+                                <h3>{{.FactText}}</h3>
                                 <img src="{{.PrimaryImage}}" width="25%" height="25%"></img>
                             </div>
                         {{end}}
@@ -43,7 +49,7 @@ var newsTemplate = `<html>
                     </html>`
 
 func main() {
-	tk := time.NewTicker(time.Second * 5)
+	tk := time.NewTicker(time.Second * 60)
 	ctx, closer := context.WithCancel(context.Background())
 	defer closer()
 	go func(c context.Context) {
@@ -61,14 +67,39 @@ func main() {
 	}(ctx)
 
 	http.HandleFunc("/", PingHandler)
-	http.HandleFunc("/fact", PollFactHandler)
-	log.Fatal(http.ListenAndServe(":3902", nil))
+	http.HandleFunc("/fact", FactHandler)
+	log.Fatal(http.ListenAndServe(":9002", nil))
 }
 
-func PingHandler  (w http.ResponseWriter, r *http.Request) {
+func FactHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		PostFactHadnler(w, r)
+	case "GET":
+		PollFactHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
 	//r.URL.Query().Get("key")
 	b, _ := json.Marshal("PONG")
 	w.Write(b)
+}
+
+func PostFactHadnler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		WriteError(w)
+	}
+	fact := MF{}
+	err = json.Unmarshal(b, &fact)
+	if err != nil {
+		WriteError(w)
+	}
+	Cache.Data = append(Cache.Data, fact)
 }
 
 func PollFactHandler(w http.ResponseWriter, r *http.Request) {
@@ -99,7 +130,6 @@ func getFact() []byte {
 	return b
 }
 
-
 func RetrieveFact() error {
 	resp, err := http.Get("http://mentalfloss.com/api/facts")
 	if err != nil {
@@ -118,4 +148,12 @@ func RetrieveFact() error {
 	}
 	Cache.Data = data
 	return nil
+}
+
+func ParseFact(mf MF) *Fact {
+	return &Fact{
+		Description: mf.FactText,
+		Url:         mf.Url,
+		Image:       mf.PrimaryImage,
+	}
 }
