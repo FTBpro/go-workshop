89150195c5a71321bdf851485c3e270d4a021f06
avihay <avihay@90min.com>
2019-04-30 11:29:30 +0300

    remove entities

--- coolfacts/facts/parser.go
+++ coolfacts/facts/parser.go
@@ -2,6 +2,25 @@ package facts
 
 import "encoding/json"
 
+type MentalFlossItem struct {
+	Id            string   `json:"id"`
+	Url           string   `json:"url"`
+	FactId        string   `json:"factId"`
+	Headline      string   `json:"headline"`
+	ShortHeadline string   `json:"shortHeadline"`
+	FactText      string   `json:"fact"`
+	FullStoryUrl  string   `json:"fullStoryUrl"`
+	Tags          []string `json:"tags"`
+	PrimaryImage  string   `json:"primaryImage"`
+	ImageCredit   string   `json:"imageCredit"`
+}
+
+type Fact struct {
+	Image       string
+	Url         string
+	Description string
+}
+
 type parser struct {
 }
 
