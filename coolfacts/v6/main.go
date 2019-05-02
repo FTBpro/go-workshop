89759fdf9c1e1c30d96b368a73f2797fc89422c2
avihay <avihay@90min.com>
2019-05-02 14:58:19 +0300

    refactor main

--- coolfacts/v6/main.go
+++ coolfacts/v6/main.go
@@ -12,6 +12,10 @@ import (
 	"github.com/FTBpro/go-workshop/coolfacts/v6/mentalfloss"
 )
 
+type Handlerer struct {
+	store *fact.Store
+}
+
 var newsTemplate = `<html>
                     <h1>Facts</h1>
                     <div>
@@ -27,6 +31,20 @@ var newsTemplate = `<html>
 func main() {
 	factsStore := fact.Store{}
 	mf := mentalfloss.Mentalfloss{}
+	handlerer := &Handlerer{&factsStore}
+
+	err := fillStoreWithNewData(mf, &factsStore)
+
+	http.HandleFunc("/ping", handlerer.Ping)
+	http.HandleFunc("/facts", handlerer.Facts)
+
+	err = http.ListenAndServe(":9002", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fillStoreWithNewData(mf mentalfloss.Mentalfloss, factsStore *fact.Store) error {
 	facts, err := mf.Facts()
 	if err != nil {
 		log.Fatal("can't reach mentalfloss: ", err)
@@ -34,64 +52,68 @@ func main() {
 	for _, f := range facts {
 		factsStore.Add(f)
 	}
+	return err
+}
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "no http handler found", http.StatusNotFound)
-			return
-		}
-		w.Header().Add("Content-Type", "text/plain")
-		_, err := fmt.Fprint(w, "PONG")
-		if err != nil {
-			errMessage := fmt.Sprintf("error writing response: %v", err)
-			http.Error(w, errMessage, http.StatusInternalServerError)
-		}
-	})
-
-	http.HandleFunc("/facts", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			w.Header().Add("Content-Type", "text/html")
-
-			tmpl, err := template.New("facts").Parse(newsTemplate)
-			if err != nil {
-				errMessage := fmt.Sprintf("error ghttp template writing: %v", err)
-				http.Error(w, errMessage, http.StatusInternalServerError)
-				return
-			}
-			tmpl.Execute(w, factsStore.GetAll())
-			return
-		}
-		if r.Method == http.MethodPost {
-			b, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				errMessage := fmt.Sprintf("error read from body: %v", err)
-				http.Error(w, errMessage, http.StatusInternalServerError)
-				return
-			}
-			var req struct {
-				Image       string `json:"image"`
-				Url         string `json:"url"`
-				Description string `json:"description"`
-			}
-			err = json.Unmarshal(b, &req)
-			if err != nil {
-				errMessage := fmt.Sprintf("error parsing fact: %v", err)
-				http.Error(w, errMessage, http.StatusBadRequest)
-			}
-
-			f := fact.Fact{
-				Image:       req.Image,
-				Url:         req.Url,
-				Description: req.Description,
-			}
-
-			factsStore.Add(f)
-			w.Write([]byte("SUCCESS"))
-		}
-	})
-
-	err = http.ListenAndServe(":9002", nil)
+func (h *Handlerer) Ping(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "no http handler found", http.StatusNotFound)
+		return
+	}
+	w.Header().Add("Content-Type", "text/plain")
+	_, err := fmt.Fprint(w, "PONG")
 	if err != nil {
-		log.Fatal(err)
+		errMessage := fmt.Sprintf("error writing response: %v", err)
+		http.Error(w, errMessage, http.StatusInternalServerError)
 	}
 }
+
+func (h *Handlerer) Facts(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		h.getFacts(w)
+		return
+	}
+	if r.Method == http.MethodPost {
+		h.postFacts(r, w)
+		return
+	}
+	http.Error(w, "no http handler found", http.StatusNotFound)
+}
+
+func (h *Handlerer) postFacts(r *http.Request, w http.ResponseWriter) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errMessage := fmt.Sprintf("error read from body: %v", err)
+		http.Error(w, errMessage, http.StatusInternalServerError)
+		return
+	}
+	var req struct {
+		Image       string `json:"image"`
+		Url         string `json:"url"`
+		Description string `json:"description"`
+	}
+	err = json.Unmarshal(b, &req)
+	if err != nil {
+		errMessage := fmt.Sprintf("error parsing fact: %v", err)
+		http.Error(w, errMessage, http.StatusBadRequest)
+	}
+	f := fact.Fact{
+		Image:       req.Image,
+		Url:         req.Url,
+		Description: req.Description,
+	}
+	h.store.Add(f)
+	w.Write([]byte("SUCCESS"))
+}
+
+func (h *Handlerer) getFacts(w http.ResponseWriter) {
+	w.Header().Add("Content-Type", "text/html")
+	tmpl, err := template.New("facts").Parse(newsTemplate)
+	if err != nil {
+		errMessage := fmt.Sprintf("error ghttp template writing: %v", err)
+		http.Error(w, errMessage, http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, h.store.GetAll())
+}
+
