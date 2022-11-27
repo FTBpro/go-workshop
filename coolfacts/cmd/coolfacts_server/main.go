c2f591de52d54995669f649b79acc12df97d8400
Oren Rosen <oren363@gmail.com>
2022-11-27 17:50:39 +0200

    add log

--- coolfacts/cmd/coolfacts_server/main.go
+++ coolfacts/cmd/coolfacts_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/FTBpro/go-workshop/coolfacts/coolfact"
@@ -9,14 +10,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, Server!")
 
 	factsRepo := inmem.NewFactsRepository()
 	service := coolfact.NewService(factsRepo)
 	server := NewServer(service)
 
-	fmt.Println("starting server on port 9002")
-	fmt.Println("You can go to http://127.0.0.1:9002/ping")
+	log.Println("starting server on port 9002")
+	log.Println("You can go to http://127.0.0.1:9002/ping")
 	if err := http.ListenAndServe(":9002", server); err != nil {
 		panic(fmt.Errorf("server crashed! err: %w", err))
 	}
