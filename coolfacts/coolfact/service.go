45b065e937f3223ac564e4b375ece3fa8c828ee1
Oren Rosen <oren363@gmail.com>
2022-11-28 15:27:05 +0200

    aaaaa

--- coolfacts/coolfact/service.go
+++ coolfacts/coolfact/service.go
@@ -1,28 +1,26 @@
-package inmem
+package coolfact
 
-import (
-	"github.com/FTBpro/go-workshop/coolfacts/coolfact"
-)
+import "fmt"
 
-type factsRepo struct {
-	facts []coolfact.Fact
+type Repository interface {
+	GetFacts() ([]Fact, error)
 }
 
-func NewFactsRepository() *factsRepo {
-	return &factsRepo{
-		facts: []coolfact.Fact{
-			{
-				Image:       "https://images2.minutemediacdn.com/image/upload/v1556645500/shape/cover/entertainment/D5aliXvWsAEcYoK-fe997566220c082b98030508e654948e.jpg",
-				Description: "Did you know sonic is a hedgehog?!",
-			},
-			{
-				Image:       "https://images2.minutemediacdn.com/image/upload/v1556641470/shape/cover/entertainment/uncropped-Screen-Shot-2019-04-30-at-122411-PM-3b804f143c543dfab4b75c81833bed1b.jpg",
-				Description: "You won't believe what happened to Arya!",
-			},
-		},
+type service struct {
+	factsRepo Repository
+}
+
+func NewService(factsRepo Repository) *service {
+	return &service{
+		factsRepo: factsRepo,
 	}
 }
 
-func (r *factsRepo) GetFacts() ([]coolfact.Fact, error) {
-	return r.facts, nil
+func (s *service) GetFacts() ([]Fact, error) {
+	facts, err := s.factsRepo.GetFacts()
+	if err != nil {
+		return nil, fmt.Errorf("factsService.GetFacts: %w", err)
+	}
+
+	return facts, nil
 }
