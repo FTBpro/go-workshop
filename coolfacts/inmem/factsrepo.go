2ba4e989b69ef2d5ac38615d4db828f93ff5ce3e
Oren Rosen <oren363@gmail.com>
2022-11-28 15:29:23 +0200

    aaaaa #2

--- coolfacts/inmem/factsrepo.go
+++ coolfacts/inmem/factsrepo.go
@@ -9,9 +9,20 @@ type factsRepo struct {
 }
 
 func NewFactsRepository() *factsRepo {
-	// TODO: init facts repo
+	return &factsRepo{
+		facts: []coolfact.Fact{
+			{
+				Image:       "https://images2.minutemediacdn.com/image/upload/v1556645500/shape/cover/entertainment/D5aliXvWsAEcYoK-fe997566220c082b98030508e654948e.jpg",
+				Description: "Did you know sonic is a hedgehog?!",
+			},
+			{
+				Image:       "https://images2.minutemediacdn.com/image/upload/v1556641470/shape/cover/entertainment/uncropped-Screen-Shot-2019-04-30-at-122411-PM-3b804f143c543dfab4b75c81833bed1b.jpg",
+				Description: "You won't believe what happened to Arya!",
+			},
+		},
+	}
 }
 
 func (r *factsRepo) GetFacts() ([]coolfact.Fact, error) {
-	// TODO: implement
+	return r.facts, nil
 }
