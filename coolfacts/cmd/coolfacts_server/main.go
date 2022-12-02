db815ae6f0734c696870f692331c0b4dcc3676ce
Oren Rosen <oren363@gmail.com>
2022-12-02 19:43:37 +0200

    add arg

--- coolfacts/cmd/coolfacts_server/main.go
+++ coolfacts/cmd/coolfacts_server/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	fmt.Println("Hello, Server!")
 
-	factsRepo := inmem.NewFactsRepository()
+	factsRepo := inmem.NewFactsRepository(seedFacts()...)
 	service := coolfact.NewService(factsRepo)
 	server := NewServer(service)
 
@@ -20,5 +20,18 @@ func main() {
 	log.Println("You can go to http://127.0.0.1:9002/ping")
 	if err := http.ListenAndServe(":9002", server); err != nil {
 		panic(fmt.Errorf("server crashed! err: %w", err))
+	}
+}
+
+func seedFacts() []coolfact.Fact {
+	return []coolfact.Fact{
+		{
+			Topic:       "Games",
+			Description: "Did you know sonic is a hedgehog?!",
+		},
+		{
+			Topic:       "TV",
+			Description: "You won't believe what happened to Arya!",
+		},
 	}
 }
